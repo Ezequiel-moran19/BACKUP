39ad8281c48196767316d6f6245e3fd0312cf72a
moran-eze <e.moran@sistemas-utnfra.com.ar>
2025-10-30 00:32:56 -0300

    agregue ejs para login y dashboard

--- PROYECTO/BACKOFFICE/server.js
+++ PROYECTO/BACKOFFICE/server.js
@@ -1,29 +1,25 @@
 import express from "express";
 import path from "path";
+import router from "./src/routers/index.js";
+import { PORT } from "./src/config/puerto.js";
 
 const app = express();
-const PORT = 8081;
 
-// Middleware
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Carpeta de vistas
+// Configuración EJS
 app.set("view engine", "ejs");
 app.set("views", path.resolve("src/views"));
 
-// Archivos estáticos (css, js, img)
+// Archivos estáticos
 app.use(express.static(path.resolve("src/public")));
 
 // Rutas
-app.get("/login", (req, res) => res.render("login"));
+app.use("/", router);
 
-app.post("/login", (req, res) => {
-  const { nombre, pass } = req.body;
-  if (nombre === "admin" && pass === "1234") return res.redirect("/dashboard");
-  res.send("Usuario o contraseña incorrecta ❌");
-});
-
-app.get("/dashboard", (req, res) => res.render("dashboard"));
-
-app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
+// El servidor ⬇⬇
+app.listen(PORT, () =>
+  console.log(`Servidor en http://localhost:${PORT}`)
+);
--- PROYECTO/BACKOFFICE/src/routers/index.js
+++ PROYECTO/BACKOFFICE/src/routers/index.js
@@ -1,12 +1,21 @@
 import express from "express";
-const router = express.Router()
+const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.render("login")
-})
+// Redirige al login
+router.get("/", (req, res) => res.redirect("/login"));
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+// Renderiza el login
+router.get("/login", (req, res) => res.render("login"));
+
+// Validación login
+router.post("/login", (req, res) => {
+  const { nombre, pass } = req.body;
+  if (nombre === "admin" && pass === "1234")
+    return res.redirect("/dashboard");
+  res.send("Usuario o contraseña incorrecta ❌");
 });
 
-export default router
+// Dashboard
+router.get("/dashboard", (req, res) => res.render("dashboard"));
+
+export default router;
